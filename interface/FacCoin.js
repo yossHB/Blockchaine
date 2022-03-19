0f77cc085b569aa39fc0759987299032711968a3
yossHB <yyosr00@gmail.com>
2022-03-19 01:36:01 +0100

    js functions

--- interface/FacCoin.js
+++ interface/FacCoin.js
@@ -1,15 +1,23 @@
-
-function transfer(){
+function transfere(){
+    let transfer = document.getElementById("destinationaddress").value;
+    let value = document.getElementById("value").value;
+    document.getElementById("ResultTransfer").innerHTML = "Result : done";
 
 }
 function transferFrom(){
-
+    let admin = document.getElementById("fromadminaddress").value;
+    let destination = document.getElementById("fromdestinationaddress").value;
+    let value = document.getElementById("fromvalue").value;
+    document.getElementById("ResultTransferFrom").innerHTML = "Result : done";
 }
 
 function balanceOf(){
-
+    let transfer = document.getElementById("balanceadminaddress").value;
+    document.getElementById("ResultBalanceOf").innerHTML = "Result : done";
 }
 
 function allowance(){
-
+    let admin = document.getElementById("allowanceadminaddress").value;
+    let destination = document.getElementById("allowancedestinationaddress").value;
+    document.getElementById("ResultAllowance").innerHTML = "Result : done";
 }
